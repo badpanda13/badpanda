0d1c4c04b7a3e307db8cb7032fe9b3517091a79c
Belka <badpanda@tut.by>
2019-05-27 15:56:22 +0300

    Initial Commit

---
+++ badpandaproj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  badpandaproj
+//
+//  Created by Belka on 5/27/19.
+//  Copyright © 2019 Belka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
