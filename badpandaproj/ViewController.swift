c5f5c1bf6025c8989a9b645ce021cc1dfff84e12
badpanda13 <badpanda@tut.by>
2019-05-28 00:26:11 +0300

    add text field inside alert popup

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -17,8 +17,12 @@ class ViewController: UIViewController {
 
     @IBAction func alertButton(_ sender: Any) {
         let alertController = UIAlertController(title: "Опрос", message: "Кто молодец??", preferredStyle: .alert)
-        let alert = UIAlertAction(title: "Белки!!!", style: .default)
-        alertController.addAction(alert)
+        let action = UIAlertAction(title: "Белки!!!", style: .default){ (action) in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil text")
+        }
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(action)
         self.present(alertController, animated: true, completion:nil)
     }
     
