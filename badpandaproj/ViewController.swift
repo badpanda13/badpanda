7f6adf492d162dc07e78e255a835fbaf5de36d7a
badpanda13 <badpanda@tut.by>
2019-05-28 16:00:36 +0300

    UIDatePicker

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -10,43 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let picker = UIPickerView()
+    let picker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         picker.center = view.center
-        
-        //подписаться
-        picker.dataSource = self
-        picker.delegate = self
-        
+       
         self.view.addSubview(picker)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-  
     
 }
 
-extension ViewController: UIPickerViewDataSource {
-    
-    //сколько компонентов выводить
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    //сколько строк в компоненте
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 3
-    }
-    
-}
-
-extension ViewController: UIPickerViewDelegate {
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let result = "row = \(row)"
-        return result
-    }
-}
