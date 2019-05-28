38f84760e88f99bc52b914cae1c57cd6003fcaf2
badpanda13 <badpanda@tut.by>
2019-05-28 13:26:42 +0300

    add switch

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mySwitch = UISwitch()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //creation of switch
+      //  self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
+        self.mySwitch.center = self.view.center
+        self.view.addSubview(mySwitch)
+        //turn this shit on
+        self.mySwitch.setOn(true, animated: true)
+        //check
+        if self.mySwitch.isOn {
+            print("switch on")
+        }else {
+            print("switch off")
+        }
+        
+        //handler
+        self.mySwitch.addTarget(self, action: #selector(switchChange(paramTarget: )), for: .valueChanged)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   @objc func switchChange(paramTarget: UISwitch){
+        print("param is = ",paramTarget)
+        if paramTarget.isOn {
+            print("switch on")
+        }else {
+            print("switch off")
+        }
+    }
+    
     @IBAction func alertButton(_ sender: Any) {
         let alertController = UIAlertController(title: "Опрос", message: "Кто молодец??", preferredStyle: .alert)
         let action = UIAlertAction(title: "Белки!!!", style: .default){ (action) in
