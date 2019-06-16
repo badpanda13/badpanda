17e9b763e29832eb32e66ce9677b8d350c5c4857
badpanda13 <badpanda@tut.by>
2019-06-16 17:14:43 +0300

    UITextField and NotificationCenter

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -9,31 +9,90 @@
 import UIKit
 //import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
 
-    let myLabel = UILabel()
+    var myTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
        // self.title = "Belka title. First VC"
         
-        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 155 )
-        myLabel.frame = labelFrame
-        myLabel.numberOfLines = 0
-        myLabel.lineBreakMode = .byWordWrapping
-        myLabel.adjustsFontSizeToFitWidth = true
-      //  myLabel.sizeToFit() // some problems with it - white screen without label
-        myLabel.textColor = UIColor.blue
-        myLabel.shadowColor = UIColor.yellow
-        myLabel.shadowOffset = CGSize(width: 10, height: 20)
-        myLabel.text = "Belka  belka belka /n belka belka belka"
-        myLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        myLabel.center = self.view.center
-        view.addSubview(myLabel)
+        var textFieldFrame = CGRect(x: 0, y: 0, width: 100, height: 31)
+        myTextField = UITextField(frame: textFieldFrame)
+        myTextField.borderStyle = .roundedRect
+        myTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        myTextField.textAlignment = .center
+        myTextField.placeholder = "belka?"
+        myTextField.center = self.view.center
+        self.view.addSubview(myTextField)
+        myTextField.delegate = self
+        
+        // Это для отлавливания изменения в текстфилде
+//        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange), name: Notification.Name.NSUbiquityIdentityDidChange, object: nil)
+     
+        
+        //это для того, чтобы при появлении клавиатуры (сейчас он у меня не появляется почему-то) , текстфилд смещался, нов  видео уроке было не UIResponder.keyboardWillShowNotification, а NSNotification.Name.UIKeyboardWillShow 
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (nc) in self.view.frame.origin.y = -200
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (nc) in self.view.frame.origin.x = 0.0
+        }
     }
     
+    @objc func textFieldDidChange(param: NSNotification){
+        
+    }
+    //UITextFieldDelegate
+    
+    @available(iOS 2.0, *)
+    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool // return NO to disallow editing.
+    {
+        return true
+    }
+    
+    @available(iOS 2.0, *)
+    public func textFieldDidBeginEditing(_ textField: UITextField) // became first responder
+    {
+
+    }
+    
+    @available(iOS 2.0, *)
+    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+    {
+        return true
+    }
+    
+    @available(iOS 2.0, *)
+    public func textFieldDidEndEditing(_ textField: UITextField) // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+    {
+        
+    }
+    
+    @available(iOS 10.0, *)
+    public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) // if implemented, called in place of textFieldDidEndEditing:
+    {
+        
+    }
+    
+    @available(iOS 2.0, *)
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool // return NO to not change text
+    {
+        return true
+    }
+    
+    @available(iOS 2.0, *)
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool // called when clear button pressed. return NO to ignore (no notifications)
+    {
+        return true
+    }
+    
+    @available(iOS 2.0, *)
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        return true
+    }
  
 
   
