801e4a39ef582f41e758968d1f61b50ed34e9d44
badpanda13 <badpanda@tut.by>
2019-06-17 00:13:21 +0300

    урок 14. UITextView

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -11,91 +11,45 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-
-    var myTextField = UITextField()
+    var myTextView = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-       // self.title = "Belka title. First VC"
+        myTextView = UITextView(frame: CGRect(x: 20, y: 100, width: self.view.bounds.width - 50, height: self.view.bounds.height / 2))
+        myTextView.text = "belka belak blka belka belka"
+        myTextView.backgroundColor = UIColor.green
+        myTextView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        myTextView.font = UIFont.systemFont(ofSize: 17)
+        self.view.addSubview(myTextView)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView), name: UIResponder.keyboardDidShowNotification , object: nil)
+    }
+    
+    //NSNotification.Name.UIKeyboardDidShow
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.myTextView.resignFirstResponder();
+        self.myTextView.backgroundColor = UIColor.white
         
-        var textFieldFrame = CGRect(x: 0, y: 0, width: 100, height: 31)
-        myTextField = UITextField(frame: textFieldFrame)
-        myTextField.borderStyle = .roundedRect
-        myTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        myTextField.textAlignment = .center
-        myTextField.placeholder = "belka?"
-        myTextField.center = self.view.center
-        self.view.addSubview(myTextField)
-        myTextField.delegate = self
-        
-        // Это для отлавливания изменения в текстфилде
-//        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange), name: Notification.Name.NSUbiquityIdentityDidChange, object: nil)
-     
-        
-        //это для того, чтобы при появлении клавиатуры (сейчас он у меня не появляется почему-то) , текстфилд смещался, нов  видео уроке было не UIResponder.keyboardWillShowNotification, а NSNotification.Name.UIKeyboardWillShow 
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (nc) in self.view.frame.origin.y = -200
+    }
+    
+    
+    //чтобы появилась в симуляторе мобильная клавиатура, надо нажать в симуляторе шифт + команд + к
+    @objc func updateTextView(param: NSNotification){
+        let userInfo = param.userInfo
+        //        let getKeyboadrRect = (userInfo![UIKeyboadrFrameAndUserInfoKey] as! NSValue).cgRectValueA
+        var getKeyboadrRect  = (userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardFrame = self.view.convert(getKeyboadrRect, to: view.window)
+        if param.name == UIResponder.keyboardWillHideNotification {
+            //если клавиатура прячется
+            myTextView.contentInset = UIEdgeInsets.zero
+        }else {
+            //еслик лавиатура появляется
+            myTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.height, right: 0)
+            myTextView.scrollIndicatorInsets = myTextView.contentInset
         }
         
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (nc) in self.view.frame.origin.x = 0.0
-        }
+        myTextView.scrollRangeToVisible(myTextView.selectedRange)
     }
-    
-    @objc func textFieldDidChange(param: NSNotification){
-        
-    }
-    //UITextFieldDelegate
-    
-    @available(iOS 2.0, *)
-    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool // return NO to disallow editing.
-    {
-        return true
-    }
-    
-    @available(iOS 2.0, *)
-    public func textFieldDidBeginEditing(_ textField: UITextField) // became first responder
-    {
-
-    }
-    
-    @available(iOS 2.0, *)
-    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
-    {
-        return true
-    }
-    
-    @available(iOS 2.0, *)
-    public func textFieldDidEndEditing(_ textField: UITextField) // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
-    {
-        
-    }
-    
-    @available(iOS 10.0, *)
-    public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) // if implemented, called in place of textFieldDidEndEditing:
-    {
-        
-    }
-    
-    @available(iOS 2.0, *)
-    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool // return NO to not change text
-    {
-        return true
-    }
-    
-    @available(iOS 2.0, *)
-    public func textFieldShouldClear(_ textField: UITextField) -> Bool // called when clear button pressed. return NO to ignore (no notifications)
-    {
-        return true
-    }
-    
-    @available(iOS 2.0, *)
-    public func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
-    {
-        return true
-    }
- 
-
-  
-    
 }
 
