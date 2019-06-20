07bc2dc72232f793efc45762a31994097c22884f
badpanda13 <badpanda@tut.by>
2019-06-21 00:43:20 +0300

    UIScrollView part 1

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -8,24 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     
-    @IBOutlet var buttons: [UIButton]!
+    var myScrollView = UIScrollView()
+    var myImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imageToLoad = UIImage(named: "1.jpg")
+        myImageView = UIImageView(image: imageToLoad)
+        myScrollView = UIScrollView(frame: self.view.bounds)
+        myScrollView.addSubview(myImageView)
+        myScrollView.contentSize = self.myImageView.bounds.size
+        myScrollView.delegate = self
+        self.view.addSubview(myScrollView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-    @IBAction func buttonChange(_ sender: Any) {
-        let button = sender as! UIButton
-        if button.titleLabel?.text == "but1" {
-            print("BUT1")
-        } else if button.tag == 1 {
-            print("BUT2")
-        }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("Начинается прокрутка")
+        print(scrollView.contentOffset.y)
+        self.myScrollView.alpha = 0.50
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("Вызывается после прокрутки")
+        self.myScrollView.alpha = 1.0
+    }
+  
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        print("Гарантирует, что вернет альфа 1")
+          self.myScrollView.alpha = 1.0 
     }
 }
 
