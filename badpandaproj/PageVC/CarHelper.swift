8d20b2c0404f01749b64b563d96c4a8069dce99b
badpanda13 <badpanda@tut.by>
2019-06-25 18:42:04 +0300

    UIPageViewController + lazy + Constraints  //сборка автомобилей и слайсом меняем картинки и названия

---
+++ badpandaproj/PageVC/CarHelper.swift
@@ -0,0 +1,15 @@
+//
+//  CarHelper.swift
+//  badpandaproj
+//
+//  Created by Belka on 6/25/19.
+//  Copyright © 2019 Belka. All rights reserved.
+//
+
+import UIKit
+
+struct CarHelper {
+    var name = String()
+    var image = UIImage()
+}
+
