cce4bd1dadd3cca81fc7510b42d6c1408b885083
badpanda13 <badpanda@tut.by>
2019-06-26 14:23:05 +0300

    UIDynamicAnimator + UICollision +UIGravity + UIPushBehavior. // кубик начинал двигаться при клике в сторону клика

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     //UIGravityBehavior - имитация гравитации
     //UIPushBehavior - реагирует на толчки
     //UISnapBehavior - крепит view к определенной точке
- //   let squareView = UIView()
+
     var animator = UIDynamicAnimator()
-    var squareView = [UIDynamicItem]()
- 
+    var squareView = UIView()
+    var pushBehavior = UIPushBehavior()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,76 +26,51 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        
-        let numberOfView = 2
-        squareView.reserveCapacity(numberOfView)
-        var colors = [UIColor.red, UIColor.green]
-        var currentCenterPoint: CGPoint = view.center
-        let eachViewSize = CGSize(width: 50, height: 50)
-        
-        for counter in 0..<numberOfView {
-            let newView = UIView(frame: CGRect(x: 0, y: 0, width: eachViewSize.width, height: eachViewSize.height))
-            newView.backgroundColor = colors[counter]
-            newView.center = currentCenterPoint
-            currentCenterPoint.y += eachViewSize.height + 10
-            view.addSubview(newView)
-            squareView.append(newView)
-        }
-        
-        animator = UIDynamicAnimator(referenceView: view)
-        
-        //Создадим тяготение
-        let gravity = UIGravityBehavior(items: squareView)
-        animator.addBehavior(gravity)
-        
-        //реализуем столкновение
-        let collision = UICollisionBehavior(items: squareView)
-        collision.translatesReferenceBoundsIntoBoundary = true //нижняя граница вью будет сдерживать наши вьюшки
-        collision.addBoundary(withIdentifier: "bottomBoundary" as NSCopying, from: CGPoint(x: 0, y: view.bounds.size.height - 100), to: CGPoint(x: view.bounds.size.width, y: view.bounds.size.height - 100))
-        
-        animator.addBehavior(collision)
-        collision.collisionDelegate = self
-        
-        /* создаем зеленый кубик и от под гравитацией падает
-            let squareView = UIView()
-        //создаем квадрат
-        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+       createGestureRecognixer()
+        createSmallSquareView()
+        createAnimationAndBehavior()
+    }
+    
+    //создаем квадрат
+    func createSmallSquareView(){
+        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80 ))
         squareView.backgroundColor = UIColor.green
-        squareView.center = self.view.center
+        squareView.center = view.center
         view.addSubview(squareView)
-        
-        //создаем аниматор и тяготение
+    }
+    
+//    создаем жест
+    func createGestureRecognixer(){
+        let tapGestureRecognixer = UITapGestureRecognizer(target: self, action: #selector(hendlerTap))
+        view.addGestureRecognizer(tapGestureRecognixer)
+    }
+    
+    //создадим столкновение
+    func createAnimationAndBehavior(){
         animator = UIDynamicAnimator(referenceView: view)
-        let gravity = UIGravityBehavior(items: [squareView])
-        animator.addBehavior(gravity)
-         */
+        //создаем столкновение
+        let collision = UICollisionBehavior(items: [squareView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        pushBehavior = UIPushBehavior(items: [squareView], mode: UIPushBehavior.Mode.continuous)
+        animator.addBehavior(collision)
+        animator.addBehavior(pushBehavior)
     }
-}
-
-
-
-//когда кубик сталкивается, он изменяется и пропадает
-    extension ViewController: UICollisionBehaviorDelegate {
-        func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
-            let indentifier = identifier as? String
-            let kbottomBoundary = "bottomBoundary"
-            if indentifier == kbottomBoundary {
-                
-                UIView.animate(withDuration: 1.0, animations: {
-                    let view = item as? UIView
-                    view?.backgroundColor = UIColor.blue
-                    view?.alpha = 0.0
-                    view?.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-                    
-                }, completion: {(finished) in
-                    let view = item as? UIView
-                    behavior.removeItem(item)
-                    view?.removeFromSuperview()
-                    
-                })
-            }
+    
+    
+    @objc func hendlerTap(paramTap: UITapGestureRecognizer){
+        //получаем угол view
+        let tapPoint: CGPoint = paramTap.location(in: view)
+        let squareViewCenterPoint: CGPoint = squareView.center
+        //определение угла между двумя точками: arc tangent 2(( p1.x - p2.x), (p1.y - p2.y))
+        let deltaX: CGFloat = tapPoint.x - squareViewCenterPoint.x
+        let deltaY: CGFloat = tapPoint.y - squareViewCenterPoint.y
+        let angle: CGFloat = atan2(deltaY, deltaX)
+        pushBehavior.angle = angle
         
-        }
+        let distanceBetweenPints:CGFloat = sqrt(pow(tapPoint.x - squareViewCenterPoint.x, 2.0) + pow(tapPoint.y - squareViewCenterPoint.y, 2.0))
+        pushBehavior.magnitude = distanceBetweenPints / 200
     }
+    
+}
 
 
