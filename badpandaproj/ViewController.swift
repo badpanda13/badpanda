f02eb2cbf38f46eb378ecb42c6521f82888183bb
badpanda13 <badpanda@tut.by>
2019-07-04 17:56:03 +0300

    UISnap // нажимаем и квадратик как по сметане с телепанием едет на точку касания

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -9,68 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // UIAttachment -  ghbdzprf
-    // UISnapBehavior - снимок
+    // UISnapBehavior - делает снимок
     
     var squareView = UIView()
-    var squareViewAnchor = UIView()
-    var anchorView = UIView()
     var animator = UIDynamicAnimator()
-    var attachmentBehavior: UIAttachmentBehavior?
+    var snapBehavior: UISnapBehavior?
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         createGestureRecognizer()
-    
-        creaeSmallSquare()
-             createAnchorView()
-        createAnimationAnBehavior()
+        createSquare()
+        createAnimatorBehavior()
+        
     }
     
-    //сщздаем квадрат
-    func creaeSmallSquare(){
-        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
-        squareView.backgroundColor = UIColor.yellow
-        squareView.center = self.view.center
-        squareViewAnchor = UIView(frame: CGRect(x: 60, y: 0, width: 20, height: 20))
-        squareViewAnchor.backgroundColor = UIColor.brown
-        squareView.addSubview(squareViewAnchor)
-        self.view.addSubview(squareView)
-    }
-    
-    //view с точкой привязки
-    func createAnchorView(){
-        anchorView = UIView(frame: CGRect(x: 120, y: 120, width: 20, height: 20))
-        anchorView.backgroundColor = UIColor.red
-        self.view.addSubview(anchorView)
-    }
-    
-    //создаем регистратор жеста панорамирования
     func createGestureRecognizer(){
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        view.addGestureRecognizer(panGestureRecognizer)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGestureRecognizer)
     }
     
-    //создаем столкновение и прикрепление
-    func createAnimationAnBehavior(){
+    func createSquare(){
+        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        squareView.center = self.view.center
+        squareView.backgroundColor = UIColor.green
+        view.addSubview(squareView)
+    }
+    
+    func createAnimatorBehavior(){
         animator = UIDynamicAnimator(referenceView: view)
-        //столкновения
-        let collision = UICollisionBehavior(items: [squareView ])
+        let collision = UICollisionBehavior(items: [squareView])
         collision.translatesReferenceBoundsIntoBoundary = true
-        // создает границы, за которые обхект не выпалет
-        attachmentBehavior = UIAttachmentBehavior(item: squareView, attachedToAnchor: anchorView.center)
         animator.addBehavior(collision)
-        animator.addBehavior(attachmentBehavior!)
+        
+        //зацеп
+        snapBehavior = UISnapBehavior(item: squareView, snapTo: squareView.center)
+        snapBehavior?.damping = 0.5
+        animator.addBehavior(snapBehavior!)
+        
     }
     
-    // определяет, где палц и передает красный квадрат туда
-    @objc func handlePan(param: UIPanGestureRecognizer){
-       
-        //передать локейшен пальца на вью
+    @objc func handleTap(param: UITapGestureRecognizer){
+        //получает угол между точкой касания и view
         let tapPoint = param.location(in: view)
-        print(tapPoint)
-        attachmentBehavior?.anchorPoint = tapPoint
-        anchorView.center = tapPoint
+        if snapBehavior != nil {
+            animator.removeBehavior(snapBehavior!)
+            
+        }
+        snapBehavior = UISnapBehavior(item: squareView, snapTo: tapPoint)
+        snapBehavior?.damping = 0.5 //средняя осциляция
+        animator.addBehavior(snapBehavior!)
         
         
         
