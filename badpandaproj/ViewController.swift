a799bdb803eda100977efe0e452952159339a467
badpanda13 <badpanda@tut.by>
2019-07-09 18:47:05 +0300

    UITableViewController part 1 // при рефреше тейбл вью, добавляются новые строки с датой

--- badpandaproj/ViewController.swift
+++ badpandaproj/ViewController.swift
@@ -10,79 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    //видео урок где-то со второй трети показывает как работать в сториборде https://www.youtube.com/watch?v=Qmu_GvVwCi0
+    let tableViewControler = UITableViewController(style: .plain)
+    var refresh = UIRefreshControl()
     
-    @IBOutlet weak var myTableView: UITableView!
-    let identifier = "myCell"
-    var array = ["1","2","3","4","5","6","7"]
+    var allTime = [Date]() //["Robbins", "Jobs", "Tanya Khiz"]
+    var cellIdentifier = "Cell"
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        
-       
-}
-    
-    
-    @IBAction func editTable(_ sender: Any) {
-        myTableView.isEditing = !myTableView.isEditing
+        createTableView()
+        self.refresh.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        view.addSubview(tableViewControler.tableView)
+        tableViewControler.tableView.addSubview(refresh)
     }
     
-
+    @objc func handleRefresh(){
+        allTime.append(Date())
+        refresh.endRefreshing()
+        let indexPathNewRow = IndexPath(row: allTime.count - 1, section: 0)
+        tableViewControler.tableView.insertRows(at: [indexPathNewRow], with: .automatic)
+    }
+    
+    func createTableView(){
+        tableViewControler.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableViewControler.tableView.delegate = self
+        tableViewControler.tableView.dataSource = self
+        
+    }
 }
+
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        return allTime.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        let number = array[indexPath.row]
-        cell.textLabel?.text = number
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let date = allTime[indexPath.row]
+        cell.textLabel?.text = "\(date)"
+        
         return cell
     }
     
-    //delete
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
-    }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            array.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .left)
-        }
-    }
     
-    //move
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let item = array[sourceIndexPath.row]
-        array.remove(at: sourceIndexPath.row)
-        array.insert(item, at: destinationIndexPath.row)
-    }
-    
-    func tableView(_ tableView: UITableView, shouldShowMenuForRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, canPerformAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        print(action)
-        if action == #selector(copy(_:)){
-            return true
-        }
-        return false
-    }
-    
-    func tableView(_ tableView: UITableView, performAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) {
-        if action == #selector(copy(_:)){
-            let cell = tableView.cellForRow(at: indexPath)
-            let pasteBoard = UIPasteboard.general
-            pasteBoard.string = cell?.textLabel?.text
-        }
-    }
 }
 
